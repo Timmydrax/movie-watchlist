834671a129b9bb138b2d3026190db6c243cbca1a
Timmydrax <timmydrax@gmail.com>
2025-07-15 03:03:50 +0100

    FEAT: Watchlist feat.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiKey = "4edfe651";
 const movieTitle = document.getElementById("movie-input");
 const form = document.getElementById("form");
 const movieContainer = document.getElementById("movie-container");
+const watchListContainer = document.getElementById("watchlist-container");
 
 function renderMovie(e) {
   e.preventDefault();
@@ -26,7 +27,7 @@ function renderMovie(e) {
          <span class="movie-genre">
           <p>${data.Runtime}</p>
           <p>${data.Genre}</p>
-        <button><i class="fa-solid fa-plus"></i>Watchlist</button>
+        <button id="add-button"><i class="fa-solid fa-plus"></i>Watchlist</button>
          </span>
         <p class="plot">${data.Plot}</p>
       </div>
@@ -35,8 +36,34 @@ function renderMovie(e) {
 
       console.log(data);
       movieContainer.innerHTML = renderHtml;
+
+      // Add to watch list
+      const addToWatchlist = document.getElementById("add-button");
+
+      if (addToWatchlist) {
+        addToWatchlist.addEventListener("click", () => {
+          // Watchlist Array
+          let watchList = JSON.parse(localStorage.getItem("watchList")) || [];
+          // To check if movie has been already added
+          const alreadyAdded = watchList.some(
+            (movie) => movie.imdbID === data.imdbID
+          );
+
+          if (!alreadyAdded) {
+            watchList.push(data);
+            localStorage.setItem("watchlist", JSON.stringify(watchList));
+            console.log("Movie has been added to watchlist!");
+          } else {
+            console.log("Don't bother, movie already in watchlist!");
+          }
+        });
+      }
     })
     .catch((error) => console.error(`The error: ${error}`));
 }
 
 form.addEventListener("submit", renderMovie);
+
+document.getElementById("add-button").addEventListener("click", () => {
+  console.log("added to watchlist");
+});
